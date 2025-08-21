35a4d731fd84a047581a7198058f6aa5bbcb18eb
久富章平 <hisadomi@hisatomishouheinoMacBook-Air.local>
2025-08-21 19:54:27 +0900

    solve a problem

---
+++ abc419/a.py
@@ -0,0 +1,16 @@
+S = input()
+
+atcode_lang = ""
+if S == "red":
+    atcode_lang = "SSS"
+
+elif S == "blue":
+    atcode_lang = "FFF"
+
+elif S == "green":
+    atcode_lang = "MMM"
+
+else:
+    atcode_lang = "Unknown"
+
+print(atcode_lang)
